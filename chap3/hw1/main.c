07ee1966727437eca8b4b7eff0998ef7abecd554
kyungho <llls101022@gmail.com>
2024-09-29 05:33:34 +0000

    modify main

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -2,27 +2,36 @@
 #include <string.h>
 #include "copy.h"
 
-char line[MAXLINE];
-char longest[MAXLINE];
+#define LINE 5
+char line[LINE][MAXLINE];
+char temp[MAXLINE];
 
 int main(){
-	int len;
+	int len,pre_len;
 	int max;
-	int i;
+	int i,j;
 	max = 0;
 	i = 0 ;
 
-	while(i <=4 && gets(line) != NULL){
-		len = strlen(line);
-
-		if(len > max) {
-			max = len;
-			copy(line, longest);
+	while(i <=4 && gets(line[i]) != NULL){
+		len = strlen(line[i]);
+        
+		for(j=0;j<i;j++)
+		{
+			pre_len = strlen(line[j]);
+			if( len > pre_len)
+			{
+				copy(line[i], temp);
+				copy(line[j], line[i]);
+				copy(temp, line[j]);
+			}
 		}
+	
 		i ++;
 	}
-	if (max > 0)
-		printf("%s \n", longest);
-
+	for(i=0;i<=4;i++)
+	{
+		printf("%s \n", line[i]);
+	}
 	return 0;
 }

deec11c28a5c499098edad46a10125bb49998f47
kyungho <llls101022@gmail.com>
2024-10-22 15:40:51 +0000

    modify chap4

--- chap4/hw2/main.c
+++ chap4/hw2/main.c
@@ -4,15 +4,30 @@ int main(int argc, char *argv[])
 {
 	FILE *fp;
 	int c;
-	if (argc < 2)
-		fp = stdin;
-	else fp = fopen(argv[1],"r");
+	char lin[256][256];
+	int n;
 
-	c = getc(fp);
-	while(c != EOF)
+	for(int i=1;i<argc;i++)
 	{
-		putc(c, stdout);
-		c = getc(fp);
+		if(argv[i][0] == '-')
+		{
+			if(argv[i][1] == 'n')
+				n=1;
+		}
+		else
+		{
+			fp = fopen(argv[i],"r");
+			fgets(lin[i],sizeof(lin),fp);
+		}
+	}
+
+	for(int i=1;i<argc;i++)
+	{
+		if(i==1&&n==1)
+			i+=1;
+		if(n==1)
+			printf("%3d ",i-1);
+		printf("%s",lin[i]);
 	}
 	fclose(fp);
 	return 0;
